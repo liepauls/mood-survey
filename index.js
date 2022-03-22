4fa3e991d4f5a91d63ed153d607e0e86d9016d58
Pauls Liepa <liepauls@gmail.com>
2022-03-22 08:20:02 +0200

    Change wording, add surveys

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('button').addEventListener('click', () => {
     const surveys = [
-      'city',
-      'nature',
-      'mixed'
+      'https://docs.google.com/forms/d/e/1FAIpQLSc-jC5WBQXeHqLIrhxv3ROWG1VF-eGqmQVxL8uf20yXSJkQEw/viewform?usp=sf_link',
+      'https://docs.google.com/forms/d/e/1FAIpQLScTTKxSCyCLOoDuCX018xjxmGyNx21Q55WDtTINROMvVQ4Sdg/viewform?usp=sf_link',
+      'https://docs.google.com/forms/d/e/1FAIpQLSf8jwlUvSloejO9TGbodzgGlUgGIaSG3hLRM16GnshKyUv7MQ/viewform?usp=sf_link'
     ]
 
     const survey = surveys[Math.floor(Math.random() * surveys.length)]
 
-    window.location = window.location
+    window.location = survey
   })
 })
