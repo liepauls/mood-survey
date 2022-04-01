9998a83ab9adb95770b1aaa117803fbeadebecd0
Pauls Liepa <liepauls@gmail.com>
2022-04-01 13:53:03 +0300

    Increase chance to randomize specific surveys

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const surveys = [
       'https://docs.google.com/forms/d/e/1FAIpQLSc-jC5WBQXeHqLIrhxv3ROWG1VF-eGqmQVxL8uf20yXSJkQEw/viewform?usp=sf_link',
       'https://docs.google.com/forms/d/e/1FAIpQLScTTKxSCyCLOoDuCX018xjxmGyNx21Q55WDtTINROMvVQ4Sdg/viewform?usp=sf_link',
+      'https://docs.google.com/forms/d/e/1FAIpQLScTTKxSCyCLOoDuCX018xjxmGyNx21Q55WDtTINROMvVQ4Sdg/viewform?usp=sf_link',
+      'https://docs.google.com/forms/d/e/1FAIpQLSf8jwlUvSloejO9TGbodzgGlUgGIaSG3hLRM16GnshKyUv7MQ/viewform?usp=sf_link',
       'https://docs.google.com/forms/d/e/1FAIpQLSf8jwlUvSloejO9TGbodzgGlUgGIaSG3hLRM16GnshKyUv7MQ/viewform?usp=sf_link'
     ]
 
